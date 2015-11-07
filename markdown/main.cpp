16f484ced3e133dec93381e5eaed5641df8811d8
Kevin Hira <kevindhira@gmail.com>
2015-11-08 01:42:28 +1300

    coded writing of header and footer of html document

--- markdown/main.cpp
+++ markdown/main.cpp
@@ -7,8 +7,29 @@
 //
 
 #include <iostream>
+#include <cstdio>
 
-int main(int argc, const char * argv[]) {
+void Indent(FILE *f, int n)
+{
+    for (int i=0; i<n; i++) {
+        fprintf(f, "\t");
+    }
+}
+
+int main(int argc, const char * argv[])
+{
+    FILE *outFile, *markdownFile;
+    char header[] = "<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<title>{TEST}</title>\n\t</head>\n\t<body>\n";
+    char footer[] = "\t</body>\n</html>";
+    int indent = 2;
+    if (argc<3 || (outFile=fopen(argv[1], "w"))==NULL || (markdownFile=fopen(argv[2], "r"))==NULL)
+        return 1;
+    
+    fprintf(outFile, "%s", header);
+    
+    
+    fprintf(outFile, "%s", footer);
+    
     
     return 0;
 }
